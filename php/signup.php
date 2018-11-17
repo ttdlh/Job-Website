966fe093546e2df32c4cc486a090485ecdeb7d89
root <root@localhost.localdomain>
2018-11-17 15:51:59 +0200

    signup_inprog

--- php/signup.php
+++ php/signup.php
@@ -8,30 +8,36 @@
   if(isset($_POST['signup'])){
     require_once('database_connection.php');
 
-    if(isset($_POST['username'])){
-      if(isset($_POST['password'])){
-        if(isset($_POST['cpassword'])){
-          if(isset($_POST['email'])){
-            if(isset($_POST['phone'])){
-              if(isset($_POST['address']){
-                echo 'success';
+    $query="select username from users";
+    $response= mysqli_query($dbc, $query);
+    if(!(mysqli_fetch_array($response))){
+      if($_POST['username'] != ''){
+        if($_POST['password'] != ''){
+          if($_POST['cpassword'] != ''){
+            if($_POST['email'] != ''){
+              if(isset($_POST['phone'])){
+                if($_POST['address'] != ''){
+                  echo 'success';
+                }else{
+                  echo '<p>Please enter your address</p>';
+                }
               }else{
-                echo '<p>Please enter your address</p>';
+                echo '<p>Please enter your phone number</p>';
               }
             }else{
-              echo '<p>Please enter your phone number</p>';
+              echo '<p>Please enter your email</p>';
             }
           }else{
-            echo '<p>Please enter your email</p>';
+            echo '<p>Please confirm the password</p>';
           }
         }else{
-          echo '<p>Please confirm the password</p>';
+          echo '<p>Please enter a password</p>';
         }
       }else{
-        echo '<p>Please enter a password</p>';
+        echo '<p>Please enter a username</p>';
       }
     }else{
-      echo '<p>Please enter a username</p>';
+      echo 'user already exist';
     }
   }
 ?>
