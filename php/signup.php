bd088c2385cccd287b72b5ad889438d496773dad
root <root@localhost.localdomain>
2018-11-17 15:20:32 +0200

    signup err

--- php/signup.php
+++ php/signup.php
@@ -8,8 +8,30 @@
   if(isset($_POST['signup'])){
     require_once('database_connection.php');
 
-    if(isset($_POST['']))
-    $_POST
-
+    if(isset($_POST['username'])){
+      if(isset($_POST['password'])){
+        if(isset($_POST['cpassword'])){
+          if(isset($_POST['email'])){
+            if(isset($_POST['phone'])){
+              if(isset($_POST['address']){
+                echo 'success';
+              }else{
+                echo '<p>Please enter your address</p>';
+              }
+            }else{
+              echo '<p>Please enter your phone number</p>';
+            }
+          }else{
+            echo '<p>Please enter your email</p>';
+          }
+        }else{
+          echo '<p>Please confirm the password</p>';
+        }
+      }else{
+        echo '<p>Please enter a password</p>';
+      }
+    }else{
+      echo '<p>Please enter a username</p>';
+    }
   }
 ?>
