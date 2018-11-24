7d341ba30a76cefca24079ce16c17a0588ef3cfa
HaythamTahboub <45122427+HaythamTahboub@users.noreply.github.com>
2018-11-24 16:51:14 +0200

    Update details.php

--- details.php
+++ details.php
@@ -1,3 +1,22 @@
+<?PHP 
+  function getUserIP() { // Get real visitor IP behind CloudFlare network 
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) { 
+      $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"]; 
+      $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"]; 
+    } 
+    $client = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR']; 
+    $remote = $_SERVER['REMOTE_ADDR']; 
+    if(filter_var($client, FILTER_VALIDATE_IP)) { 
+      $ip = $client; 
+    } elseif(filter_var($forward, FILTER_VALIDATE_IP)) { 
+    $ip = $forward; 
+    } else { 
+      $ip = $remote; 
+    } return $ip; 
+  }
+  $GLOBALS['user_ip']=getUserIP();
+?>
 <!doctype html><!-- not done -->
 <html>
 <head>
@@ -13,6 +32,9 @@
   require_once("html/nav.php");
   ?>
   <div class="container">
+    <p>
+      Hello <?php echo $GLOBALS['user_ip']; ?>
+    </p>
     <ul class="list-group">
       <?phprequire_once("php/database_connection.php");
 
